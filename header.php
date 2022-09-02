9f15b7525f1492695289ffe8dc899ca6b8eb72df
Scott Llewellyn <scott@thinkds.org>
2022-09-02 13:35:22 +0100

    Add favicons and fix fonts

--- header.php
+++ header.php
@@ -75,7 +75,7 @@
   <!-- so:fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=optional" rel="stylesheet">
   <!-- eo:fonts -->
 
   <!-- so:favicon -->
